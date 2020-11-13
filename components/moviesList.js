d9a6227c7388ce8623dd1ba60eb0150c59faf99e
Antonio Rossi <a.rossi@jayex.com>
2020-11-13 08:55:40 +0000

    added movie list component

---
+++ components/moviesList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+class MovieList extends Component {
+  render() {
+    return(
+      <SafeAreaView>
+        <Text>I'm a movies list</Text>
+      </SafeAreaView>
+    )
+  }
+}
+
+export default MovieList;
